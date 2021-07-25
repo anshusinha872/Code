98caa6c348014276e6cd57a9a7ae8281ed618f75
Anshu Sinha <anshusinha872@gmail.com>
2021-07-25 13:33:12 +0530

    code

--- c++/0-1KnapSap_recursion.cpp
+++ c++/0-1KnapSap_recursion.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
-int knapSap(int weight[],int value[],int W,int N)
+int knapSack(int weight[],int value[],int W,int N)
 {
     if(N==0 || W==0)
         return 0;
     if(weight[N]>W)
-        knapSap(weight,value,W,N-1);
+        knapSack(weight,value,W,N-1);
     if(weight[N]<=W)
-        return max(knapSap(weight,value,W-weight[N],N-1) + value[N],knapSap(weight,value,W,N-1));
+        return max(knapSack(weight,value,W-weight[N],N-1) + value[N],knapSack(weight,value,W,N-1));
 }
 int main()
 {
@@ -18,6 +18,6 @@ int main()
         cin>>weight[i];
     for(int i=1;i<n+1;i++)
         cin>>value[i];
-    cout<<knapSap(weight,value,w,n);
+    cout<<knapSack(weight,value,w,n);
     return 0;
 }
